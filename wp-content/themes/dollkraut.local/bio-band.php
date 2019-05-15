348e73ec1863b24ee83635c59f68f46c64351ad0
neluss38 <nielst2000@gmail.com>
2019-05-15 16:35:39 +0200

    updated header and changed updated bio content

--- wp-content/themes/dollkraut.local/bio-band.php
+++ wp-content/themes/dollkraut.local/bio-band.php
@@ -1,7 +1,5 @@
 <div data-index="4" class="bio-content bio-band page">
     <div class="bio-content__text-wrapper">
-        <p class="bio-content__text">
-            <?php the_field('bio_band'); ?>
-        </p>
+        <?php the_field('bio_band'); ?>
     </div>
 </div>
--- wp-content/themes/dollkraut.local/bio-dj.php
+++ wp-content/themes/dollkraut.local/bio-dj.php
@@ -1,7 +1,5 @@
 <div data-index="3" class="bio-content bio-dj page">
     <div class="bio-content__text-wrapper">
-        <p class="bio-content__text">
-            <?php the_field('bio_dj'); ?>
-        </p>
+        <?php the_field('bio_dj'); ?>
     </div>
 </div>
