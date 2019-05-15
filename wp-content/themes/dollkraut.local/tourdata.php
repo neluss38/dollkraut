408e7fff115a009cd9616ed42e694d19135fc401
neluss38 <nielst2000@gmail.com>
2019-05-15 16:54:10 +0200

    changed iframe quotes

--- wp-content/themes/dollkraut.local/tourdata.php
+++ wp-content/themes/dollkraut.local/tourdata.php
@@ -1,3 +1,3 @@
 <div data-index="1" class="tour-data page">
-    <iframe src=“https://www.residentadvisor.net/widget/eventlisting?dj=dollkraut&bgcolor=282425&linkcolor=93119f&textcolor=93119f&darkbuttons=true” height=“640" width=“640” frameborder=“0" />
+    <iframe src="https://www.residentadvisor.net/widget/eventlisting?dj=dollkraut&bgcolor=282425&linkcolor=93119f&textcolor=93119f&darkbuttons=true" height="640" width="640" frameborder="0" />
 </div>
