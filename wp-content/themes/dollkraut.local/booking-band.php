b91467d51a47a70963a294176781696421a03505
neluss38 <nielst2000@gmail.com>
2020-02-19 11:46:11 +0100

    vs code test

--- wp-content/themes/dollkraut.local/booking-band.php
+++ wp-content/themes/dollkraut.local/booking-band.php
@@ -1,13 +1,16 @@
 <div data-index="5" class="info-wrapper booking-band page">
     <div class="info-wrapper__book">
         <div class="info-wrapper__book__flex">
-            <a href="http://www.dollkraut.com" target="_blank" rel="noopener" class="info-wrapper__book-image info-wrapper__book-image--voyage"></a>
+            <a href="<?php the_field('band_netherlands_url');?>" target="_blank" rel="noopener" class="info-wrapper__book-image info-wrapper__book-image--voyage"></a>
             <a href="mailto:jorn@voyage-select.com" target="_blank" rel="noopener" class="info-wrapper__book-text"><?php the_field('booking_band_netherlands'); ?></a>
         </div>
     </div>
+    <p>hoi</p>
+    <p>2</p>
+    <p>3</p>
     <div class="info-wrapper__book info-wrapper__book--swamp">
         <div class="info-wrapper__book__flex">
-            <a href="http://www.dollkraut.com" target="_blank" rel="noopener" class="info-wrapper__book-image info-wrapper__book-image--swamp"></a>
+            <a href="<?php the_field('band_europe_url');?>" target="_blank" rel="noopener" class="info-wrapper__book-image info-wrapper__book-image--swamp"></a>
             <a href="mailto:ricky@swampbooking.de" target="_blank" rel="noopener" class="info-wrapper__book-text"><?php the_field('booking_band_europe_worldwide'); ?></a>
         </div>
     </div>
